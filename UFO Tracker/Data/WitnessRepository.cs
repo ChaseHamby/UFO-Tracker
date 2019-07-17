10f462af8d1d689a7479172cf0138f93d7ab0481
unknown <c.chase.hamby@gmail.com>
2019-07-17 13:24:27 -0500

    witness controller, model, and repo done. HttpGet works

---
+++ UFO Tracker/Data/WitnessRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UFO_Tracker.Data
+{
+    public class WitnessRepository
+    {
+        const string ConnectionString = "Server=localhost; Database=UFO-Tracker; Trusted_Connection=True;";
+
+        public IEnumerable<Witness> GetAll()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var witnesses = db.Query<Witness>("Select * from Witnesses").ToList();
+
+                return witnesses;
+            }
+        }
+    }
+}
