779dca108d80f1f0f0080ec3b2b5f8b6ae693e1f
unknown <c.chase.hamby@gmail.com>
2019-07-17 13:12:14 -0500

    locations controller, data, and model done and HttpGet works

---
+++ UFO Tracker/Controllers/LocationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UFO_Tracker.Data;
+
+namespace UFO_Tracker.Controllers
+{
+    [Route("api/[controller]")]
+    public class LocationController : ControllerBase
+    {
+        readonly LocationRepository _locationRepository;
+        public LocationController()
+        {
+            _locationRepository = new LocationRepository();
+        }
+
+        [HttpGet]
+
+        public ActionResult GetAllLocations()
+        {
+            var locations = _locationRepository.GetAll();
+
+            return Ok(locations);
+        }
+    }
+}
---
+++ UFO Tracker/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UFO_Tracker.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public float CityLongitude { get; set; }
+        public float CityLatitude { get; set; }
+    }
+}
